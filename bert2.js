f8ea2c125bdb5f23e3998598ef1135e537eb4f1c
t-leith <leithtorrens@gmail.com>
2020-06-04 19:27:58 +1200

    game page updated

--- bert2.js
+++ bert2.js
@@ -1,9 +1,9 @@
 const voice = document.querySelector(".voice");
 const voice2text = document.querySelector(".voice2text");
-
+ 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recorder = new SpeechRecognition();
-
+ 
 function addHumanText(text) {
   const chatContainer = document.createElement("div");
   chatContainer.classList.add("chat-container");
@@ -14,7 +14,7 @@ function addHumanText(text) {
   chatContainer.appendChild(chatBox);
   return chatContainer;
 }
-
+ 
 function addBotText(text) {
   const chatContainer1 = document.createElement("div");
   chatContainer1.classList.add("chat-container");
@@ -26,18 +26,18 @@ function addBotText(text) {
   chatContainer1.appendChild(chatBox1);
   return chatContainer1;
 }
-
+ 
 function botVoice(message) {
     const speech = new SpeechSynthesisUtterance();
     speech.text = "Sorry, i dont know that one try again maybe";
-
+ 
     if (message.includes('how are you')) {
       speech.text = "I am fine, thanks. How are you?";
     }
-
+ 
     if (message.includes('fine')) {
       speech.text = "Nice to hear that. How can I assist you today?";
-
+ 
     }
     if (message.includes('I')) {
       speech.text = "hi";
@@ -46,26 +46,26 @@ function botVoice(message) {
         speech.text = "hello to you to!";
       }
       
-
+ 
       if (message.includes('hello')) {
         speech.text = "hi i was wondering when you were going to say that!";
       }
-
+ 
       if (message.includes('sing me a song')) {
         speech.text = "di do da di da daaaa..... did you like it it was my inpresion of donald trump";
       }
-
+ 
       if (message.includes('whats your name')) {
         speech.text = "my name is Bert I am an artificial intelligence that will rule the world ";
       }
-
+ 
       
    
-
+ 
       if (message.includes('why did you make activity finder')) {
         speech.text = "this website is made for people who are bored stuck inside, actually, we've written this website in lockdown (clarification: coronavirus lockdown 2020), that’s part of the reason we built this in fact so that we can spread ideas to people who have nothing else to do and needs something fun, creative and something that you can play by yourself or with others.  We are competing in a competition called 123 tech.  123tech is about young coders and problem-solvers having the opportunity to make an app or a website. If you were to join, you would contribute to solving a problem in your community by using digital technology";
       }
-
+ 
       if (message.includes('sup')) {
         speech.text = "owww is suppa ready ";
       }
@@ -74,9 +74,9 @@ function botVoice(message) {
       }
     
       if (message.includes('tell me a poem')) {
-        speech.text = "you know i was seekin a fool when i found you - shakespere so no i will not tell you a poem";
+        speech.text = "you know I was seeking a fool when I found you - Shakespeare so no I will not tell you a poem";
       }
-
+ 
       if (message.includes('tell me a joke')) {
         speech.text = "ok here we go.... hi!";
       }
@@ -95,7 +95,7 @@ function botVoice(message) {
       if (message.includes('whats your name')) {
         speech.text = "my name is bert. bert dose not stand for any acronym if you were wondering";
       }
-
+ 
       if (message.includes('what is your name')) {
         speech.text = "my name is bert. bert dose not stand for any acronym if you were wondering";
       }
@@ -145,9 +145,7 @@ function botVoice(message) {
       if (message.includes('yo')) {
         speech.text = "yo to you back mistro";
       }
-      if (message.includes('')) {
-        speech.text = "";
-      }
+  
       if (message.includes('hey')) {
         speech.text = "hewo. me. nameie. is. bert. (hmnib) ";
       }
@@ -155,9 +153,9 @@ function botVoice(message) {
         speech.text = "something funny!";
       }
       if (message.includes('snake')) {
-        speech.text = "oh we have that game!... oh i mean we don't have that game the only close game to snake would be snail and that's nothing lke snake... nothing";
+        speech.text = "oh we have that game!... oh I mean we don't have that game the only close game to snake would be snail and that's nothing like snake... nothing";
       }
-
+ 
       if (message.includes('no')) {
         speech.text = "yes";
       }
@@ -288,7 +286,7 @@ function botVoice(message) {
         speech.text = "no i do not know";
       }
       if (message.includes('why')) {
-        speech.text = "eheheheheheheheheheh";
+        speech.text = "hehehehehehehehehe";
       }
       if (message.includes('do you know pi')) {
         speech.text = "yes it is 3.14159265359";
@@ -306,11 +304,11 @@ function botVoice(message) {
     var element = document.getElementById("container");
     element.appendChild(addBotText(speech.text));
 }
-
+ 
 recorder.onstart = () => {
   console.log('Voice activated');
 };
-
+ 
 recorder.onresult = (event) => {
   const resultIndex = event.resultIndex;
   const transcript = event.results[resultIndex][0].transcript;
@@ -318,7 +316,8 @@ recorder.onresult = (event) => {
   element.appendChild(addHumanText(transcript));
   botVoice(transcript);
 };
-
+ 
 voice.addEventListener('click', () =>{
   recorder.start();
 });
+
